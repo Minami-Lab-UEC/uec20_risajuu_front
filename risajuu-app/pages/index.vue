717076484211d8b8d76fbb9d74395ea9a4ef6de1
naoki matsuura <m2010629@gl.cc.uec.ac.jp>
2023-05-06 18:10:53 +0900

    add : voicevox Co-authored-by: hyoutan <hyoutan07@users.noreply.github.com>

--- risajuu-app/pages/index.vue
+++ risajuu-app/pages/index.vue
@@ -33,12 +33,49 @@ export default defineComponent({
         .post("http://localhost:8080/api/v1/chat", {
           text: this.message,
           emotion: true,
+          voicevox: true,
+        },{
+          // responseType: 'arraybuffer' // レスポンスのデータタイプを指定する
         })
         .then((response: any) => {
-          console.log(response);
-          this.messageList.push(response.data.response);
-          document.getElementById("emotionButton")?.click(); // 疑似的に隠されている感情を変更するボタンを押す
+          console.log("レスポンス", response.data);
+          //this.messageList.push(response.data.response);
+          // const jsonBlob = new Blob([response.data[0]], { type: 'application/json' });
+          // const blob = new Blob([response.data], { type: 'audio/wav' });
+
+          // // BlobオブジェクトをURLに変換して、音声を再生する
+          // const url = URL.createObjectURL(blob);
+          // const audio = new Audio(url);
+          // audio.play();
+
+          const audioData = response.data.file
+          const decodedAudioData = atob(audioData)
+          const arrayBuffer = new ArrayBuffer(decodedAudioData.length)
+          const view = new Uint8Array(arrayBuffer)
+          for (let i = 0; i < decodedAudioData.length; i++) {
+            view[i] = decodedAudioData.charCodeAt(i)
+          }
+          const blob = new Blob([arrayBuffer], { type: 'audio/wav' })
+          const audioUrl = URL.createObjectURL(blob)
+          const audio = new Audio(audioUrl)
+          audio.play()
+
+          // レスポンスJSONデータを取得する
+          // const responseData = response.headers;
+          // console.log(responseData);
+          console.log("レスポンス", response.data.data);
+  
+          // document.getElementById("emotionButton")?.click(); // 疑似的に隠されている感情を変更するボタンを押す
         })
+        // .then(response => response.formData())
+        // .then(formData => {
+        //   const jsonFile = formData.get('json_file');
+        //   const wavFile = formData.get('wav_file');
+        //   document.getElementById("emotionButton")?.click(); // 疑似的に隠されている感情を変更するボタンを押す
+
+        //   console.log(jsonFile);
+        //   console.log(wavFile);
+        // });
         .catch((error: any) => {
           console.log(error);
         });

51b9e43f52c33a120145b4aff6b8fbc65206f6f6
yamashi4423 <yamashi4423@gmail.com>
2023-05-06 13:29:40 +0900

    create: APIから得られた感情をモデルに渡す

--- risajuu-app/pages/index.vue
+++ risajuu-app/pages/index.vue
@@ -5,38 +5,46 @@
     </div>
     <input type="text" v-model="message" />
     <button id="test" @click="sendMessage">送信</button>
+
+    <div>
+      <input id="emotion" type="text" value="Joy" />
+      <input id="emotionStrength" type="text" value="0.65" />
+      <button id="emotionButton"></button>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
-import axios from 'axios'
+import { defineComponent } from "vue";
+import axios from "axios";
 
 export default defineComponent({
   data() {
     return {
-      message: '',
-      messageList: [''],
-    }
+      message: "",
+      messageList: [""],
+    };
   },
   methods: {
     sendMessage() {
-      alert(this.message)
+      alert(this.message);
       this.messageList.push(this.message);
-      axios.post('http://localhost:8080/api/v1/chat', {
-        text: this.message,
-        emotion: true
-      })
-      .then((response: any) => {
-        console.log(response)
-        this.messageList.push(response.data.response);
-      })
-      .catch((error: any) => {
-        console.log(error)
-      })
-    }
-  }
-})
+      axios
+        .post("http://localhost:8080/api/v1/chat", {
+          text: this.message,
+          emotion: true,
+        })
+        .then((response: any) => {
+          console.log(response);
+          this.messageList.push(response.data.response);
+          document.getElementById("emotionButton")?.click(); // 疑似的に隠されている感情を変更するボタンを押す
+        })
+        .catch((error: any) => {
+          console.log(error);
+        });
+    },
+  },
+});
 </script>
 
 <style>
